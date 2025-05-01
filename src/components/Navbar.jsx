651fce5e85f3bfd37d6d831da27c21c682d79393
POWER <161963414+Power200319@users.noreply.github.com>
2025-05-01 22:34:33 +0700

    Fix import path to use alias for Linux compatibility

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Button } from "./Ui/button";
+import { Button } from "@/components/ui/button";
 import logo from "../assets/SWIMMING_logo.png";
 
 const Navbar = () => {
