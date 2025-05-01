1a9411c86ee96cebaec28abbc18b13c9d27416be
POWER <161963414+Power200319@users.noreply.github.com>
2025-05-01 23:41:36 +0700

    change all code

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
-import { TooltipProvider } from "./components/ui/tooltip";
+import { TooltipProvider } from "./components/ui/Tooltip";
 import Index from "./page/index";
 
 const queryClient = new QueryClient();
