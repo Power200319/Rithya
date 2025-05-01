44bd5e6b390b4fde0fdbcbfb66c9f69bd08eb5fd
POWER <161963414+Power200319@users.noreply.github.com>
2025-05-01 23:53:04 +0700

    delete tooltie

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
-import { TooltipProvider } from "./components/ui/Tooltip";
+// import { TooltipProvider } from "./components/ui/Tooltip";
 import Index from "./page/index";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
+    {/* <TooltipProvider> */}
       <Toaster />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
         </Routes>
       </BrowserRouter>
-    </TooltipProvider>
+    {/* </TooltipProvider> */}
   </QueryClientProvider>
 );
 
